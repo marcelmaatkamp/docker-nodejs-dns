e65f56ff8d77e566a73226e90b116e92e79a92e3
Marcel Maatkamp <m.maatkamp@gmail.com>
2016-06-28 15:49:37 +0200

    working dns version

--- js/server.js
+++ js/server.js
@@ -1,41 +1,42 @@
-var dns = require('native-dns')
-server = dns.createServer()
-var amqp = require('amqp-ts')
+var dns = require('native-dns');
+var amqp = require("amqp-ts");
 
 var rabbitmq_url = process.env.AMQP_URL || "amqp://localhost"
-
 var connection = new amqp.Connection(rabbitmq_url);
 var exchange = connection.declareExchange("dns",  'fanout', {durable: true});
+connection.completeConfiguration().then(() => {
+  console.log("succesfull connected at " +rabbitmq_url)
 
-var dns_port = process.env.DNS_PORT||53
-console.log("Starting dns server " +rabbitmq_url)
-
-server.on('request', function (request, response) {
-  console.log("["+request.address.address+"] class["+request.question[0].class+"] type["+request.question[0].type+"] "+request.question[0].name+": "+conn)
-  exchange.publish('dns', {
-    timestamp: new Date().toISOString(),
-    header: request.header,
-    question: request.question,
-    answer: request.answer,
-    authority: request.authority,
-    additional: request.additional,
-    edns_options: request.edns_options,
-    payload: request.payload,
-    edns: request.edns,
-    edns_version: request.edns_version,
-    address: request.address
+  server = dns.createServer()
+  server.on('request', function (request, response) {
+    console.log("["+request.address.address+"] class["+request.question[0].class+"] type["+request.question[0].type+"] "+request.question[0].name)
+    var message = new amqp.Message({
+      timestamp: new Date().toISOString(),
+      header: request.header,
+      question: request.question,
+      answer: request.answer,
+      authority: request.authority,
+      additional: request.additional,
+      edns_options: request.edns_options,
+      payload: request.payload,
+      edns: request.edns,
+      edns_version: request.edns_version,
+      address: request.address
+    });
+    exchange.send(message);
+    response.answer.push(dns.A({
+      name: request.question[0].name,
+      address: '127.0.0.1',
+      ttl: 600,
+    }))
+    response.send()
   })
-  response.answer.push(dns.A({
-    name: request.question[0].name,
-    address: '127.0.0.1',
-    ttl: 600,
-  }))
-  response.send()
-})
+  server.on('error', function (err, buff, req, res) {
+    console.log(err.stack)
+  })
 
-server.on('error', function (err, buff, req, res) {
-  console.log(err.stack)
-})
+  var dns_port = process.env.DNS_PORT||53
+  console.log("Starting on port " +dns_port)
+  server.serve(dns_port)
 
-console.log("Starting on port " +dns_port)
-server.serve(dns_port)
+});
